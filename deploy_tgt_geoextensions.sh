28f7c19663f69608d48163817888f8f2189d0be5
James E. Marca <james@activimetrics.com>
2016-06-14 14:19:55 -0700

    make this like tvd
    sqitch deploy dependencies via npm dependencies, with a side dish of
    dead reckoning

---
+++ deploy_tgt_geoextensions.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+tgt_db=${npm_package_config_tgt_db:-"osm"}
+tgt_host=${npm_package_config_tgt_host:-"127.0.0.1"}
+tgt_port=${npm_package_config_tgt_port:-5432}
+tgt_user=${npm_package_config_tgt_user:-""}
+
+
+SQITCH_TGT_DB_URI=${SQITCH_DB_URI:-"db:pg://${tgt_user}@${tgt_host}:${tgt_port}/$tgt_db"}
+
+
+cd node_modules/calvad_db_geoextensions/
+
+SQITCH_DB_URI=${SQITCH_TGT_DB_URI} npm run sqitch:deploy
